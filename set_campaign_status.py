b92d457c5bb3a81c7eb11e4f260228a7b3fd933f
Keenan McKenzie <keenan@mckenziemail.com>
2020-02-12 07:34:35 -0600

    clean up authorization + add get campaign and set campaign status files

---
+++ set_campaign_status.py
@@ -0,0 +1,27 @@
+from googleads import adwords
+
+campaign_id = '9328039918'
+status = 'PAUSED'
+
+
+def main(client, campaign_status):
+    # Initialize appropriate service.
+    campaign_service = client.GetService('CampaignService', version='v201809')
+    # Construct operations and add campaigns.
+    operations = [{
+        'operator': 'SET',
+        'operand': {
+            'id': campaign_id,
+            'status': campaign_status
+        }
+    }]
+    campaigns = campaign_service.mutate(operations)
+
+    for campaign in campaigns['value']:
+        print('Campaign with name "%s" and id "%s" was modified.'
+              % (campaign['name'], campaign['id']))
+
+
+if __name__ == '__main__':
+    adwords_client = adwords.AdWordsClient.LoadFromStorage('/Users/keenan/dev/googleads-python-lib/googleads.yaml')
+    main(adwords_client, status)
